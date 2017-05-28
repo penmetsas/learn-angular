7e1de7e84d8c6ce96a9279252c41dc017424402b
penmetsas <msstackpro16@gmail.com>
2017-05-27 17:40:24 -0700

    add main component

---
+++ main.component.js
@@ -0,0 +1,11 @@
+angular.
+module('myApp').
+component('main', {
+  template:`<div>
+  {{ $ctrl.message }}
+  </div>`,
+  
+  controller: function MainController(){
+      this.message =';)';
+  }
+});
